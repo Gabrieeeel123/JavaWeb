9a58b1fa9efd5ee26558d13483907de1366c85b8
Gabrieeeel123 <104031898+Gabrieeeel123@users.noreply.github.com>
2023-05-09 10:07:56 -0500

    Update Ejecucion.java

--- figuritas/src/main/java/Ejecucion.java
+++ figuritas/src/main/java/Ejecucion.java
@@ -1,4 +1,4 @@
-
+package java;
 import java.util.Scanner;
 public class Ejecucion {
     public static void main(String[] args) {
@@ -7,10 +7,12 @@ public class Ejecucion {
         Octagono octagono = new Octagono();
         Scanner s = new Scanner(System.in);
         int opcion = 0;
-        System.out.println("Bienvenido! , ingrese 1 para entrar al modulo de escaleno\n2.para el modulo de heptagono\n3.para el modulo de octagono");
+        System.out.println("Bienvenido! , ingrese 1 para entrar al modulo de 
+        escaleno\n2.para el modulo de heptagono\n3.para el modulo de octagono");
         opcion = s.nextInt();
         switch(opcion){
-            case 1: System.out.println("Ingrese la medida de la base del escaleno");
+            case 1: System.out.println("Ingrese la medida de la base 
+            del escaleno");
             escaleno.setLadoA(s.nextDouble());
             System.out.println("Ingrese la medida de la altura del escaleno");
             escaleno.setLadoB(s.nextDouble());
@@ -18,19 +20,22 @@ public class Ejecucion {
             escaleno.setLadoC(s.nextDouble());
             escaleno.calcularArea();
             escaleno.calcularPerimetro();break;
-            case 2:System.out.println("Ingrese la medida del apotema del heptagono");
+            case 2:System.out.println("Ingrese la medida del apotema
+            del heptagono");
             heptagono.setApotema(s.nextDouble());
             System.out.println("Ingrese la medida de un lado del heptagono");
             heptagono.setLado(s.nextDouble());
             heptagono.calcularArea();
             heptagono.calcularPerimetro();break;
-            case 3:System.out.println("Ingrese la medida del apotema del Octagono");
+            case 3:System.out.println("Ingrese la medida del apotema 
+            del Octagono");
             octagono.setApotema(s.nextDouble());
             System.out.println("Ingrese la medida de un lado del Octagono");
             octagono.setLado(s.nextDouble());
             octagono.calcularArea();
             octagono.calcularPerimetro(); break;
-            default: System.out.println("no especifico una opcion valida");break;
+            default: System.out.println("no especifico una opcion 
+            valida");break;
         }
         s.close();
     }
